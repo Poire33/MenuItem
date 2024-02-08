6c1c670c3de20adfb3c2dd319c2b403799f7ed39
Rupert spelled with one P <g00423530@atu.ie>
2024-02-08 11:21:35 +0000

    Created skeleton for online ordering system and made burger, pizza, and salad classes

---
+++ src/OnlineOrderingSystem.java
@@ -0,0 +1,20 @@
+public class OnlineOrderingSystem {
+    public static void main(String[] args) {
+        // Create an array or list of MenuItem objects
+        MenuItem[] menuItems = new MenuItem[] {
+
+        };
+
+        // Display details of each new item
+        for (MenuItem menuItem : menuItems) {
+            // Print out the name, description, and price for each item
+        }
+
+        // Calculate and display the total cost of the order
+        double totalCost = 0;
+        for (MenuItem menuItem : menuItems) {
+            // totalCost of all menuItem prices in an order
+        }
+        System.out.println("Total Cost: €" + totalCost);
+    }
+}
